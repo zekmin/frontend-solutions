5c468ae22dab04cd54fc43bca798623f3c7a062a
zekmin <zekmin@yahoo.com.sg>
2016-10-06 22:44:38 +0800

    Update conditional-execution.js

--- program-structure/conditional-execution.js
+++ program-structure/conditional-execution.js
@@ -3,6 +3,10 @@
  * Write a function that greets the user by name, or by saying "Hello, World!" if no name is given.
  */
 
+String.prototype.endsWith = function (str)
+                              {
+                                 return this.indexOf(str) == 0;
+                              }
 
 exports.heyBob = function (words) {
     if words.endswith("?") {
