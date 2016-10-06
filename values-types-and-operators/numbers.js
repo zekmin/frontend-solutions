477f415cce91272705004b61405acad08653f361
zekmin <zekmin@yahoo.com.sg>
2016-10-06 20:15:15 +0800

    Update numbers.js

--- values-types-and-operators/numbers.js
+++ values-types-and-operators/numbers.js
@@ -7,7 +7,7 @@ var annualLeave = 30
 var BobRemainingDays = 12
 var AliceRemainingDays = 28
 var TomRemainingDays = 19
-var average = your_answer
+var average = (BobReminaingDays + AliceRemaining Days + TomReainingDays)/3
 
 exports.average = average
 exports.annualLeave = annualLeave
