016458ac7bbddf14179ab6b197c278ec8ae9950b
zekmin <zekmin@yahoo.com.sg>
2016-10-06 22:52:35 +0800

    Update conditional-execution.js

--- program-structure/conditional-execution.js
+++ program-structure/conditional-execution.js
@@ -3,8 +3,9 @@
  * Write a function that greets the user by name, or by saying "Hello, World!" if no name is given.
  */
 
+
 exports.heyBob = function (words) {
-    if words.endswith("?") {
+    if words.endswith?('?') =  {
     return "Sure."
   }
   if words == words.toUpperCase {
