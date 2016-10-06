b7cb22fe0383eb27b8415c0f1fc01e16ab59dad2
zekmin <zekmin@yahoo.com.sg>
2016-10-06 21:09:14 +0800

    Update strings.js

--- values-types-and-operators/strings.js
+++ values-types-and-operators/strings.js
@@ -3,11 +3,13 @@
  * Write a program that greets the user by name, or by saying "Hello, World!" if no name is given.
  */
 
+
+
 exports.hello = function (name) {
-  string Name = name
+  String Name = name
   if (!name) { // if no name
     return "Hello, World!"
   } else {
-    return "Hello," + Name
+    return Name
   }
 }
